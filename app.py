530da227b8259a1a2962fef8ef8fb567aefeb989
山田亮 <shinshu.yamada0916@gmail.com>
2026-06-30 09:00:23 +0900

    基本情報ページと数学Aページを追加

--- app.py
+++ app.py
@@ -1,10 +1,64 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "勉強タイマー"
+    return """
+<h1>ようこそ</h1>
+
+<p>総勉強時間：0時間</p>
+
+<hr>
+
+<h2>基本情報</h2>
+<p>勉強時間：0時間</p>
+<p>自己評価：★★☆☆☆</p>
+
+<a href="/basic">
+    <button>詳細</button>
+</a>
+
+<hr>
+
+<h2>数学A</h2>
+<p>勉強時間：0時間</p>
+<p>自己評価：★★★☆☆</p>
+
+<a href="/mathA">
+    <button>詳細</button>
+</a>
+
+"""
+@app.route("/basic")
+def basic():
+    return"""
+    <h1>基本情報</h1>
+
+    <p>勉強時間：0時間</p>
+
+    <button>開始</button>
+    
+    <a href="/">
+        <button>ホームへ戻る</button>
+    </a>
+        
+    """
+
+@app.route("/mathA")
+def mathA():
+    return"""
+    <h1>数学A</h1>
+    
+    <p>総勉強時間：0時間</p>
+    
+    <button>開始</button>
+    
+    <a href="/">
+        <button>ホームへ戻る</button>
+    </a>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
